4b3e544c6a67e6541969100d5ab2e364c3c3098a
pmfranco77 <pablomourefr76@gmail.com>
2017-12-11 23:09:40 +0100

    Añadido nombres para IDs

--- Views/NOTA_TRABAJO/NOTA_TRABAJO_SHOWCURRENT_View.php
+++ Views/NOTA_TRABAJO/NOTA_TRABAJO_SHOWCURRENT_View.php
@@ -9,14 +9,18 @@ Muestra la tabla en detalle de la nota seleccionada
 
 */
 class NOTA_TRABAJO_SHOWCURRENT{
-     var $login; 
-    var $IdTrabajo; 
-    var $NotaTrabajo; 
+    var $login; //declaración del atributo login
+    var $Nombre; //declaración del atributo Nombre
+    var $IdTrabajo; //declaración del atributo IdTrabajo
+    var $NombreTrabajo; //declaración del atributo NombreTrabajo 
+    var $NotaTrabajo;  //declaración del atributo NotaTrabajo
 
-function __construct($tupla){
-    $this->login = $tupla['login'];
-    $this->IdTrabajo = $tupla['IdTrabajo'];
-    $this->NotaTrabajo = $tupla['NotaTrabajo'];
+function __construct($lista){
+    $this->login = $lista['login'];
+    $this->Nombre = $lista['Nombre'];
+    $this->IdTrabajo = $lista['IdTrabajo'];
+    $this->NombreTrabajo = $lista['NombreTrabajo'];
+    $this->NotaTrabajo = $lista['NotaTrabajo'];
     $this->render();
 }
 
@@ -30,7 +34,9 @@ function render(){
             <caption><?php echo $strings['Nota'] ?></caption>
                 <tr><th><?php echo $strings['Campo'] ?></th><th><?php echo $strings['Valor'] ?></th></tr>
                 <tr><th><?php echo $strings['Login'] ?></th><td><?php echo $this->login ?></td></tr>
+                <tr><th><?php echo $strings['Nombre'] ?></th><td><?php echo $this->Nombre ?></td></tr>
                 <tr><th><?php echo $strings['IdTrabajo'] ?></th><td><?php echo $this->IdTrabajo ?></td></tr>
+                <tr><th><?php echo $strings['NombreTrabajo'] ?></th><td><?php echo $this->NombreTrabajo ?></td></tr>
                 <tr><th><?php echo $strings['Nota Trabajo'] ?></th><td><?php echo $this->NotaTrabajo ?></td></tr>
         </table>
 
