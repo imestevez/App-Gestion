2db868d509364f562cefa1042adf0cb163299a82
pmfranco77 <pablomourefr76@gmail.com>
2017-12-11 23:08:48 +0100

    Añadido nombres para IDs

--- Views/NOTA_TRABAJO/NOTA_TRABAJO_DELETE_View.php
+++ Views/NOTA_TRABAJO/NOTA_TRABAJO_DELETE_View.php
@@ -9,14 +9,18 @@ Muestra la tabla de borrado de la nota seleccionada
 
 */
 class NOTA_TRABAJO_DELETE{
-    var $login; 
-    var $IdTrabajo; 
-    var $NotaTrabajo; 
+    var $login; //declaración del atributo login
+    var $Nombre; //declaración del atributo Nombre
+    var $IdTrabajo; //declaración del atributo IdTrabajo
+    var $NombreTrabajo; //declaración del atributo NombreTrabajo 
+    var $NotaTrabajo;  //declaración del atributo NotaTrabajo
 
-function __construct($tupla){
-    $this->login = $tupla['login'];
-    $this->IdTrabajo = $tupla['IdTrabajo'];
-    $this->NotaTrabajo = $tupla['NotaTrabajo'];
+function __construct($lista){
+    $this->login = $lista['login'];
+    $this->Nombre = $lista['Nombre'];
+    $this->IdTrabajo = $lista['IdTrabajo'];
+    $this->NombreTrabajo = $lista['NombreTrabajo'];
+    $this->NotaTrabajo = $lista['NotaTrabajo'];
     $this->render();
 }
 
@@ -31,13 +35,17 @@ function render(){
             <caption><?php echo $strings['Borrar nota'] ?></caption>
                 <tr><th><?php echo $strings['Campo'] ?></th><th><?php echo $strings['Valor'] ?></th></tr>
                 <tr><th><?php echo $strings['Login'] ?></th><td><?php echo $this->login ?></td></tr>
+                <tr><th><?php echo $strings['Nombre'] ?></th><td><?php echo $this->Nombre ?></td></tr>
                 <tr><th><?php echo $strings['IdTrabajo'] ?></th><td><?php echo $this->IdTrabajo ?></td></tr>
+                <tr><th><?php echo $strings['NombreTrabajo'] ?></th><td><?php echo $this->NombreTrabajo ?></td></tr>
                 <tr><th><?php echo $strings['Nota Trabajo'] ?></th><td><?php echo $this->NotaTrabajo ?></td></tr>
         </table>
 
         <form method="post" name="DELETE" action="../Controllers/NOTA_TRABAJO_Controller.php">
             <input class="del" type="text" name="login" size="<?php echo strlen($this->login); ?>" readonly value="<?php echo $this->login ?>" >
-            <input class="del" type="text" name="IdTrabajo" size="<?php echo strlen($this->IdTrabajo); ?>" readonly value="<?php echo $this->IdTrabajo ?>">
+            <input class="del" type="text" name="Nombre" size="<?php echo strlen($this->Nombre); ?>" readonly  value="<?php echo $this->Nombre ?>">
+            <input class="del" type="text" name="IdTrabajo" size="<?php echo strlen($this->IdTrabajo); ?>" readonly value="<?php echo $this->IdTrabajo ?>" >
+            <input class="del" type="text" name="NombreTrabajo" size="<?php echo strlen($this->NombreTrabajo); ?>" readonly value="<?php echo $this->NombreTrabajo ?>" >
             <input class="del" type="text" name="NotaTrabajo"  size="<?php echo strlen($this->NotaTrabajo); ?>" readonly value="<?php echo $this->NotaTrabajo ?>">
               
             <div class="accionesTable" style="margin-left: 0%; float: right; margin-right: 45%">
