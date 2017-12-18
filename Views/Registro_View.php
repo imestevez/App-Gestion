d6035e8b2f79a3bc3a45d72d687a5535bc0507b8
ivan <i.martinezestevez@gmail.com>
2017-12-18 18:11:20 +0100

    Modificado registro

--- Views/Registro_View.php
+++ Views/Registro_View.php
@@ -31,7 +31,7 @@ include '../Views/Header.php'; //header necesita los strings
          <fieldset class="add">
                 <legend style="margin-left: 30%"><?php echo $strings['Registro de usuario'] ?></legend>
            
-            <form method="post" name="ADD"  action="../Controllers/Registro_Controller.php" enctype="multipart/form-data" >
+            <form method="post" name="ADD"  action="../Controllers/Loigin_Controller.php" enctype="multipart/form-data" >
                 
                 <div id="izquierda">
                     <label for="login"><?php echo $strings['Login']?>: </label>
@@ -72,7 +72,7 @@ include '../Views/Header.php'; //header necesita los strings
                          <input type="text" name="direccion" maxlength="60" size="60"><div id="direccion" class="oculto" style="display:none"><?php echo $strings['div_direccion']?></div> <div id="direccionVacio" class="oculto" style="display:none"><?php echo $strings['div_direccion_vacio']?></div> 
                 </div>
                 <div class="acciones" style="float: right; margin-left:0%; margin-right: 50%">
-                    <a href="../Controllers/USUARIO_Controller.php?action=ADD"> <input type="image" name="action" value="ADD" src="../Views/images/confirmar.png" title="<?php echo $strings['Enviar Formulario'] ?>" onclick="return validar('ADD') && encriptar() "></a>
+                    <a href="../Controllers/Registro_Controller.php?action=ADD"> <input type="image" name="action" value="ADD" src="../Views/images/confirmar.png" title="<?php echo $strings['Enviar Formulario'] ?>" onclick="return validar('ADD') && encriptar() "></a>
                 </div>
              </form>                     
                 <div class="acciones" style="float: left;">
