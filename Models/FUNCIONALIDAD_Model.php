786ec24b9bf3486770de5bf7b5d86fc31de93727
ivan <i.martinezestevez@gmail.com>
2017-12-19 19:48:09 +0100

    Borrado Funcionalidad

--- Models/FUNCIONALIDAD_Model.php
+++ Models/FUNCIONALIDAD_Model.php
@@ -143,14 +143,29 @@ function DELETE()
 {	// se construye la sentencia sql de busqueda con los atributos de la clase
     $sql = "SELECT * FROM FUNCIONALIDAD WHERE (IdFuncionalidad = '$this->IdFuncionalidad')";
     // se ejecuta la query
-    $result = $this->mysqli->query($sql);
+    $resultado = $this->mysqli->query($sql);
+    $num_rows = mysqli_num_rows($resultado);
     // si existe una tupla con ese valor de clave
-    if ($result->num_rows == 1)
+    if ($num_rows == 1)
     {
+    $sql = "SELECT * FROM FUNC_ACCION WHERE (IdFuncionalidad = '$this->IdFuncionalidad')";
+       	if($resultado = $this->mysqli->query($sql)){
+       		// se construye la sentencia sql de borrado
+	        $sql = "DELETE FROM FUNC_ACCION WHERE (IdFuncionalidad = '$this->IdFuncionalidad')";
+	        // se ejecuta la query
+	        $result = $this->mysqli->query($sql);
+    	}
+    	$sql = "SELECT * FROM PERMISO WHERE (IdFuncionalidad = '$this->IdFuncionalidad')";
+       	if($resultado = $this->mysqli->query($sql)){
+       		// se construye la sentencia sql de borrado
+	        $sql = "DELETE FROM PERMISO WHERE (IdFuncionalidad = '$this->IdFuncionalidad')";
+	        // se ejecuta la query
+	        $result = $this->mysqli->query($sql);
+    	}
     	// se construye la sentencia sql de borrado
         $sql = "DELETE FROM FUNCIONALIDAD WHERE (IdFuncionalidad = '$this->IdFuncionalidad')";
         // se ejecuta la query
-        $this->mysqli->query($sql);
+        $result = $this->mysqli->query($sql);
         // se devuelve el mensaje de borrado correcto
         $this->lista['mensaje'] = 'Borrado correctamente'; 
 			return $this->lista;
